24c1600f5a2c21850f643028feb4bbfe30fad86d
José Queiroz <jqueiroz.av@gmail.com>
2025-07-10 17:27:47 -0300

    TextFields and calculated sum

--- src/main/java/org/example/demojavafx/gui/ViewController.java
+++ src/main/java/org/example/demojavafx/gui/ViewController.java
@@ -3,16 +3,39 @@ package org.example.demojavafx.gui;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.time.LocalDate;
+import java.util.Locale;
+
 import org.example.demojavafx.gui.util.Alerts;
 
 public class ViewController {
 
     @FXML
-    private Button btTest;
+    private TextField txtNumber1;
 
     @FXML
-    public void onBtTestAction() {
-        Alerts.showAlert("Title here", "Header here", "Some message here!", Alert.AlertType.WARNING);
+    private TextField txtNumber2;
+
+    @FXML
+    private Label lblResult;
+
+    @FXML
+    private Button btSum;
+
+    @FXML
+    public void onBtSumAction() {
+        Locale.setDefault(Locale.US);
+        try {
+            Double number1 = Double.parseDouble(txtNumber1.getText());
+            Double number2 = Double.parseDouble(txtNumber2.getText());
+            Double sum = number1 + number2;
+            lblResult.setText(String.format("%.2f", sum));
+        } catch (NumberFormatException e) {
+            Alerts.showAlert("Error", "Invalid input", "Please enter valid numbers.", Alert.AlertType.ERROR);
+        }
     }
 
 
